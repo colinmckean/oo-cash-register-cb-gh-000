73c1798f446c2306e846e1839afb7bf08ad0af1e
Colin McKean <ezikill@hotmail.com>
2017-09-11 19:03:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
     @total = 0
     @discount = discount[0]
   end
-  def add_item(item, price, *quantity)
-    self.total += price
+  def add_item(item, price, quantity=1)
+    self.total += price * quantity
   end
 end
